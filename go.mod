6e3fb1cab0e18fca163ac933926b9d451a97de11
wangbenjun <wangbenjun@gmail.com>
2020-09-16 23:57:42 +0800

    提示

--- go.mod
+++ go.mod
@@ -7,9 +7,7 @@ require (
 	github.com/google/uuid v1.1.1
 	github.com/peterh/liner v1.1.0
 	github.com/pkg/errors v0.8.1
-	github.com/smartystreets/assertions v0.0.0-20180927180507-b2de0cb4f26d
 	github.com/smartystreets/goconvey v1.6.4 // indirect
-	github.com/stretchr/testify v1.4.0
 	github.com/thinkoner/openssl v0.0.0-20191016104818-1df12dff7f46
 	github.com/tidwall/gjson v1.3.5
 	github.com/urfave/cli/v2 v2.0.0
--- pkg/zlog/command.go
+++ pkg/zlog/command.go
@@ -3,14 +3,15 @@ package zlog
 import (
 	"fmt"
 	"go-micloud/pkg/color"
+	"time"
 )
 
 func Info(msg string) {
-	fmt.Printf(color.Green("### Info: %s\n"), msg)
+	fmt.Printf(color.Green(time.Now().Format("2006-01-02 15:04:05")+" ### 提示: %s\n"), msg)
 	Logger.Info(msg)
 }
 
 func Error(msg string) {
-	fmt.Printf(color.Red("### Error: %s\n"), msg)
+	fmt.Printf(color.Red(time.Now().Format("2006-01-02 15:04:05")+" ### 错误: %s\n"), msg)
 	Logger.Error(msg)
 }
